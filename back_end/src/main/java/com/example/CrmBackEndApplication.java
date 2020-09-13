192ab380e9baf6e230f5a3698045058fc2d11d06
baixuanzi <3163059772@qq.com>
2020-09-13 16:25:45 +0800

    初始化springboot过程

---
+++ back_end/src/main/java/com/example/CrmBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrmBackEndApplication.class, args);
+    }
+
+}
---
+++ back_end/src/test/java/com/example/crmbackend/CrmBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crmbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
