e7d973389d24aec14b812156ded862a6bfe12e77
baixuanzi <3163059772@qq.com>
2020-09-13 16:37:27 +0800

    初始化springboot工程

---
+++ back_end/src/main/java/com/example/controller/Test.java
@@ -0,0 +1,12 @@
+package com.example.controller;
+
+/**
+ * @author: pengjia
+ * @date: 2020/9/13
+ * @description:
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("测试springboot工程提交");
+    }
+}
