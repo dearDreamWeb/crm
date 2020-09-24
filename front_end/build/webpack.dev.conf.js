fd2352b59c7a6104551024e0103f6fcf4a45fec7
baixuanzi <3163059772@qq.com>
2020-09-24 10:55:36 +0800

    解决项目运行报错

--- front_end/build/webpack.dev.conf.js
+++ front_end/build/webpack.dev.conf.js
@@ -68,10 +68,10 @@ const devWebpackConfig = merge(baseWebpackConfig, {
   ]
 })
 
-module.exports = new Promise((resolve, reject) = > {
+module.exports = new Promise((resolve, reject) => {
   portfinder.basePort = process.env.PORT || config.dev.port
-  portfinder.getPort((err, port) = > {
-    if(err) {
+  portfinder.getPort((err, port) => {
+    if (err) {
       reject(err)
     } else {
       // publish the new Port, necessary for e2e tests
@@ -91,6 +91,6 @@ module.exports = new Promise((resolve, reject) = > {
 
       resolve(devWebpackConfig)
     }
-  }
-)
+  })
 })
+
