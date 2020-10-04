f7f63188d8176b96dc105a78e6e20b992a09dfea
yqq <peppa_op@163.com>
2020-10-04 22:14:02 +0800

    .xml+mapper没写完

--- back_end/src/main/java/com/example/model/mapper/szOrderMapper.java
+++ back_end/src/main/java/com/example/model/mapper/szOrderMapper.java
@@ -1,8 +1,16 @@
 package com.example.model.mapper;
 
+import com.example.entity.request.szOrder;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 /*1003 yqq*/
 @Repository
 public interface szOrderMapper {
-
+    List<szOrder> szOrderList();//全部
+    szOrder queryazOrderById(int ordId);//单条
+    int addszOrder(szOrder szOrder);//增
+    int updateszOrder(szOrder szOrder);//改
+    int deleteszOrder(int ordId);//删
 }
