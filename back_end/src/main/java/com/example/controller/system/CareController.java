54271f733291cc4e0fb753a9e0d6c7fdc1123b15
刘雷敏 <3014604461@qq.com>
2020-10-23 21:23:42 +0800

    关怀连表

--- back_end/src/main/java/com/example/controller/system/CareController.java
+++ back_end/src/main/java/com/example/controller/system/CareController.java
@@ -47,7 +47,7 @@ public class CareController {
 
 
     @GetMapping("/list")
-    public ResultVo listDict(@RequestBody Care care) {
+    public ResultVo listDict( Care care) {
         return careService.listCare(care);
     }
 
